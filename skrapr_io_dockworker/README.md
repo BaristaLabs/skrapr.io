35cbe9578028c769304f2a68c01b9bb01efa2746
Sean McLellan <sean.mclellan@baristalabs.com>
2015-07-26 13:46:19 -0400

    streamlined.

--- skrapr_io_dockworker/README.md
+++ skrapr_io_dockworker/README.md
@@ -6,4 +6,5 @@ Expected env vars:
 * AWS_ACCESS_KEY_ID: AWS Access Key
 * AWS_SECRET_ACCESS_KEY: AWS Secret Access Key
 * AWS_DEFAULT_REGION: AWS Region (e.g. 'us-east-1').
-* dockWorkerQueueUrl: Url of the AWS SQS Queue.
+* dockyardQueueUrl: Url of the AWS SQS Queue.
+* dockyardBucket: name of the bucket to store downloaded resources.
--- skrapr_io_dockworker/app.js
+++ skrapr_io_dockworker/app.js
@@ -1,34 +1,137 @@
-﻿var Download = require("download");
-var Consumer = require('sqs-consumer');
+﻿var Consumer = require('sqs-consumer');
+var AWS = require("aws-sdk");
+var s3 = new AWS.S3();
+var url = require("url");
+var moment = require("moment");
+var got = require("got");
 
 var app = Consumer.create({
-    queueUrl: process.env.dockWorkerQueueUrl,
+    queueUrl: process.env.dockyardQueueUrl,
     handleMessage: function (message, done) {
-        console.log(new Date() + ": Received Message " + message.MessageId);
+        console.log("=======================================");
+        console.log(moment().format("MM/DD hh:mm:ss") + ": Received Message " + message.MessageId);
         
-        var headers = null;
+        var headers = [];
         var messageBody = JSON.parse(message.Body);
+        var urlToDownload = messageBody.Message;
+        var urlToDownloadUrl = url.parse(urlToDownload);
+        var urlToDownloadKey = urlToDownloadUrl.host + urlToDownloadUrl.pathname;
 
-        new Download()
-            .get(messageBody.Message)
-            .use(function (response, url) {
-                headers = response.headers;
+        //TODO: Test if the file already exists.
+        var checkParams = { Bucket: process.env.dockyardBucket, Key: urlToDownloadKey };
+        s3.headObject(checkParams, function (err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else console.log(data);           // successful response
+        });
+
+        var gotParams = {
+            method: "GET",
+            headers: {
+                "pragma": "no-cache",
+                "cache-control": "no-cache",
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36",
+                "Accept-Encoding": "gzip, deflate, sdch",
+                "Accept-Language": "en-US,en; q = 0.8"
+            }
+        };
+
+        got(urlToDownloadUrl, gotParams)
+            .then(function (res) {
+                console.log(moment().format("MM/DD hh:mm:ss") + ": Downloaded " + urlToDownload + " (" + res.headers['content-type'] + ") " + res.headers['content-length']);
+
+                if (res.connection && res.connection._host)
+                    urlToDownloadKey = res.client._host + urlToDownloadUrl.pathname;        
+
+                var uploadParams = {
+                    Bucket: process.env.dockyardBucket,
+                    Key: urlToDownloadKey,
+                    Body: res.body,
+                    ContentType: res.headers['content-type'],
+                    Metadata: {
+                        originMessageId: message.MessageId,
+                        originUrl: urlToDownload,
+                        retrievedOn: Date.now().toString()
+                    }
+                };
+
+                var uploadOptions = { partSize: 10 * 1024 * 1024, queueSize: 1 };
+                s3.upload(uploadParams, uploadOptions, function (err, data) {
+                    if (err) {
+                        done(err);
+                    }
+                    else {
+                        console.log(moment().format("MM/DD hh:mm:ss") + ": Stored " + urlToDownload + " as " + data.Location);
+                        done();
+                    }
+                });
             })
-            .run(function (err, files) {
-            
-            if (err) {
-                console.log(new Date() + ": Error retrieving " + messageBody.Message);
+            .catch(function (err) {
+                console.log(moment().format("MM/DD hh:mm:ss") + ": Error retrieving " + urlToDownload);
                 console.log(err);
                 done(err);
-            }
-            else {
-                console.log(new Date() + ": Downloaded " + files.length + " files");
-                files.forEach(function (file) {
-                    console.log("\t" + file.url + "\t" + headers['content-type']);
-                })
-                done();
-            }
-        });
+            });
+
+        //new Download({
+        //    agent: tunnel.httpOverHttp({
+        //        proxy: {
+        //            host: 'localhost',
+        //            port: 8888
+        //        }
+        //    }),
+        //    method: "GET",
+        //    headers: {
+        //        "host": myHost,
+        //        "pragma": "no-cache",
+        //        "cache-control": "no-cache",
+        //        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        //        "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36",
+        //        "Accept-Encoding": "gzip, deflate, sdch",
+        //        "Accept-Language": "en-US,en; q = 0.8"
+        //    }
+        //})
+        //    .get(urlToDownload)
+        //    .use(function (response, currentUrl) {
+        //    headers.push(response.headers);
+        //    myHost = url.parse(response.headers.location).host;
+        //})
+        //    .run(function (err, files) {
+            
+        //    if (err) {
+        //        console.log(moment().format("MM/DD hh:mm:ss") + ": Error retrieving " + urlToDownload);
+        //        console.log(err);
+        //        done(err);
+        //    }
+        //    else {
+        //        console.log(moment().format("MM/DD hh:mm:ss") + ": Downloaded " + files.length + " files");
+                
+        //        files.forEach(function (file) {
+        //            console.log("\t" + file.url + "\t" + headers['content-type'] + " " + file._contents.length);
+        //            var uploadParams = {
+        //                Bucket: "skrapr-io-dockyard",
+        //                Key: urlToDownloadKey,
+        //                Body: file._contents,
+        //                ContentType: headers['content-type'],
+        //                Metadata: {
+        //                    originMessageId: message.MessageId,
+        //                    originUrl: file.url,
+        //                    retrievedOn: Date.now().toString()
+        //                }
+        //            };
+                    
+        //            var uploadOptions = { partSize: 10 * 1024 * 1024, queueSize: 1 };
+        //            s3.upload(uploadParams, uploadOptions, function (err, data) {
+        //                if (err) {
+        //                    done(err);
+        //                }
+        //                else {
+        //                    console.log(moment().format("MM/DD hh:mm:ss") + ": Stored " + file.url + " as " + data.Location);
+        //                    done();
+        //                }
+        //            });
+        //        });
+        //    }
+        //});
     }
 });
 
