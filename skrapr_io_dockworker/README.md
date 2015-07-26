42b78f88d598adb1f6e5a5d4da6a5f250fe10520
Sean McLellan <sean.mclellan@baristalabs.com>
2015-07-26 00:55:13 -0400

    got the dock worker, well, moving cargo around.

--- skrapr_io_dockworker/README.md
+++ skrapr_io_dockworker/README.md
@@ -1,3 +1,9 @@
 ﻿# skrapr_io_dockworker
 
 
+Expected env vars:
+
+AWS_ACCESS_KEY_ID: AWS Access Key
+AWS_SECRET_ACCESS_KEY: AWS Secret Access Key
+dockWorkerQueueRegion: AWS Region (e.g. 'us-east-1').
+dockWorkerQueueUrl: Url of the AWS SQS Queue.
--- skrapr_io_dockworker/app.js
+++ skrapr_io_dockworker/app.js
@@ -1,1 +1,39 @@
-﻿console.log('Hello world');
+﻿var Download = require("download");
+var Consumer = require('sqs-consumer');
+
+var app = Consumer.create({
+    region: process.env.dockWorkerQueueRegion,
+    queueUrl: process.env.dockWorkerQueueUrl,
+    handleMessage: function (message, done) {
+        console.log(new Date() + ": Received Message " + message.MessageId);
+        
+        var headers = null;
+        new Download()
+            .get(message.Body)
+            .use(function (response, url) {
+                headers = response.headers;
+            })
+            .run(function (err, files) {
+            
+            if (err) {
+                console.log(new Date() + ": Error retrieving " + message.Body);
+                console.log(err);
+                done(err);
+            }
+            else {
+                console.log(new Date() + ": Downloaded " + files.length + " files");
+                files.forEach(function (file) {
+                    console.log("\t" + file.url + "\t" + headers['content-type']);
+                })
+                done();
+            }
+        });
+    }
+});
+
+app.on('error', function (err) {
+    console.log(err.message);
+});
+
+console.log("Listening...");
+app.start();
