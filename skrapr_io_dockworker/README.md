7314ba167a45376ed125754157a4ffb324b7cf17
Sean McLellan <sean.mclellan@baristalabs.com>
2015-07-26 08:19:05 -0400

    updated to parse SNS subscription

--- skrapr_io_dockworker/README.md
+++ skrapr_io_dockworker/README.md
@@ -5,5 +5,5 @@ Expected env vars:
 
 * AWS_ACCESS_KEY_ID: AWS Access Key
 * AWS_SECRET_ACCESS_KEY: AWS Secret Access Key
-* dockWorkerQueueRegion: AWS Region (e.g. 'us-east-1').
+* AWS_DEFAULT_REGION: AWS Region (e.g. 'us-east-1').
 * dockWorkerQueueUrl: Url of the AWS SQS Queue.
--- skrapr_io_dockworker/app.js
+++ skrapr_io_dockworker/app.js
@@ -2,21 +2,22 @@
 var Consumer = require('sqs-consumer');
 
 var app = Consumer.create({
-    region: process.env.dockWorkerQueueRegion,
     queueUrl: process.env.dockWorkerQueueUrl,
     handleMessage: function (message, done) {
         console.log(new Date() + ": Received Message " + message.MessageId);
         
         var headers = null;
+        var messageBody = JSON.parse(message.Body);
+
         new Download()
-            .get(message.Body)
+            .get(messageBody.Message)
             .use(function (response, url) {
                 headers = response.headers;
             })
             .run(function (err, files) {
             
             if (err) {
-                console.log(new Date() + ": Error retrieving " + message.Body);
+                console.log(new Date() + ": Error retrieving " + messageBody.Message);
                 console.log(err);
                 done(err);
             }
